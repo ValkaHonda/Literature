f2228cfd6e2d6c4eeff040da8a4b00f56d9ebe4c
Lenny <valentin805@gmail.com>
2019-03-24 10:21:37 +0200

    Move navigation to separate module.

--- client/App.js
+++ client/App.js
@@ -1,48 +1,12 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; 
-
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen,
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
-
-const AppContainer = createAppContainer(RootStack);
+import  Routes  from './src/navigation/routes';
 
 export default class App extends Component {
   render() {
     return (
-      <AppContainer />
+      <Routes />
     );
   }
 }
---
+++ client/src/navigation/routes.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; 
+
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const Routes = createAppContainer(RootStack);
+export default Routes;
