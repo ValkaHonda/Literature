3008c3759a6ac645318249aca2733ddb2bfa0e6f
ValkaHonda <valentin805@gmail.com>
2019-04-14 08:25:18 +0300

    Establish successful connection with the server

--- mobile/src/navigation/screens/HomeScreen.js
+++ mobile/src/navigation/screens/HomeScreen.js
@@ -2,8 +2,37 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button, AppRegistry, TextInput } from 'react-native';
 import LogIn from './../../components/loginComponent.js';
 
-
 export default class HomeScreen extends Component {
+  url = {
+    base: 'https://thawing-eyrie-26509.herokuapp.com'
+  };
+  state = {
+    email: '',
+    pass: ''
+  };
+
+  onEmailChange = e => {
+      this.setState({ email: e.target.value });
+  };
+
+  onPassChange = e => {
+      this.setState({ pass: e.target.value });
+  };
+
+  sendRequest = () => {
+      fetch(`${this.url.base}`)
+      .then((response) => {
+          console.log(response);
+          this.props.navigation.push('Details');
+      })
+      .catch((error) => {
+          console.log(error);
+          this.props.navigation.push('Tests');
+      });
+  };
+  onLogInButtonPress = () => {
+    this.sendRequest();
+  };
   static navigationOptions = {
     title: 'Home',
     headerStyle: {
@@ -20,11 +49,20 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Typical Log-In Screen.</Text>
-        <LogIn></LogIn>
+        <Text>Typical Log-In Screen</Text>
+        <Text>Email:</Text>
+        <TextInput
+            style={styles.textBox}
+            onChange={this.onEmailChange}
+        />
+        <Text>Password:</Text>
+        <TextInput
+            style={styles.textBox}
+            onChange={this.onPassChange}
+        />
         <Button
           title="Log In"
-          onPress={() => this.props.navigation.push('Details')}
+          onPress={this.onLogInButtonPress}
         />
       </View>
     );
@@ -38,7 +76,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffff00',
   },
-  login: {
-    backgroundColor: '#ffffff',
-  },
+  textBox: {
+    height: 40, 
+    width: 200,
+    borderColor: 'gray', 
+    borderWidth: 1, 
+    backgroundColor: "#ffffff",
+},
 });
