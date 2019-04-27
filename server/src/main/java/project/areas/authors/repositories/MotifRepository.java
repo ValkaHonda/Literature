828e55349dc2e4900becc1fbd504f923ff6af2f4
ValkaHonda <valentin805@gmail.com>
2019-04-27 17:52:15 +0300

    Add findAllbyAuthor in MotifRepository.

--- server/src/main/java/project/areas/authors/repositories/MotifRepository.java
+++ server/src/main/java/project/areas/authors/repositories/MotifRepository.java
@@ -1,6 +1,7 @@
 package project.areas.authors.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.areas.authors.entities.Author;
 import project.areas.authors.entities.Motif;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.List;
 public interface MotifRepository extends JpaRepository<Motif,Integer> {
     List<Motif> findAllBy();
     Motif getMotifById(final Integer id);
+    List<Motif> findAllByAuthor(final Author author);
 
 }
