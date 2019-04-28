abc50ceccebb54d381b18d7cf177ea2596014425
ValkaHonda <valentin805@gmail.com>
2019-04-28 07:27:51 +0300

    Separate Work end point into WorkController.

--- server/src/main/java/project/areas/authors/controllers/WorkController.java
+++ server/src/main/java/project/areas/authors/controllers/WorkController.java
@@ -5,32 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import project.areas.authors.dto.ShowAuthorDTO;
 import project.areas.authors.dto.ShowWorkDTO;
 import project.areas.authors.entities.Author;
 import project.areas.authors.services.AuthorService;
 import project.areas.authors.services.WorkService;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/authors")
-public class AuthorController {
+public class WorkController {
     private AuthorService authorService;
     private WorkService workService;
 
     @Autowired
-    public AuthorController(final AuthorService authorService, final WorkService workService) {
+    public WorkController(final AuthorService authorService, final WorkService workService) {
         this.authorService = authorService;
         this.workService = workService;
     }
-
-    @GetMapping("/{id}")
-    public ShowAuthorDTO getAuthorById(@PathVariable("id") Integer authorId ){
-        return this.authorService.findById(authorId);
-    }
-
-    @GetMapping("")
-    public List<ShowAuthorDTO> all(){
-        return this.authorService.findAll();
+    @GetMapping("/{id}/works")
+    public List<ShowWorkDTO> getAllWorksByAuthorId(@PathVariable("id") final Integer authorId){
+        Author foundAuthor = this.authorService.findEntityById(authorId);
+        return this.workService.showAllByAuthor(foundAuthor);
     }
 }
