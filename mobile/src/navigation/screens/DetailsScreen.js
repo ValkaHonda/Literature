2a31d7eee79f01f70f293e08077b104af2eda874
Puffle5 <elena_haruno@abv.bg>
2019-05-16 07:08:08 +0300

    Implement authors voice commmand.

--- mobile/src/navigation/screens/DetailsScreen.js
+++ mobile/src/navigation/screens/DetailsScreen.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {Alert, StyleSheet, Text, View, Button, ImageBackground, Image} from 'react-native';
+import {Alert, StyleSheet, Text, View, Button, ImageBackground, Image, TouchableWithoutFeedback} from 'react-native';
 import {StoreGlobal } from '../../../App.js';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import AndroidTextToSpeech from 'react-native-tts';
+import SpeechAndroid from 'react-native-android-voice';
 
 
 
@@ -22,27 +22,48 @@ export default class DetailsScreen extends Component {
   
   }
 
+  record = async () => {
+    try{
+      //More Locales will be available upon release.
+      const spokenText = await SpeechAndroid.startSpeech("Моля, говорете", SpeechAndroid.BULGARIAN);
+      const command = spokenText.toString().toLowerCase();
+      //Alert.alert(JSON.stringify(spokenText));
+      if(command === 'автор' || command === 'автори'){
+        this.props.navigation.push('Authors')
+      } else {
+        Alert.alert('Моля, пробвайте отново');
+      }
+  }catch(error){
+    Alert.alert(JSON.stringify(error));
+      /*switch(error){
+          case SpeechAndroid.E_VOICE_CANCELLED:
+              ToastAndroid.show("Voice Recognizer cancelled" , ToastAndroid.LONG);
+              break;
+          case SpeechAndroid.E_NO_MATCH:
+              ToastAndroid.show("No match for what you said" , ToastAndroid.LONG);
+              break;
+          case SpeechAndroid.E_SERVER_ERROR:
+              ToastAndroid.show("Google Server Error" , ToastAndroid.LONG);
+              break;
+          /*And more errors that will be documented on Docs upon release*/
+      }
+  
+  };
   render() {
     return (
+      
       <ImageBackground 
       source={require('../../images/Moleskin.png')}
       style={styles.container}>
-      
-      <Image style={[{width: 232.32, height: 258.94, alignSelf: 'center'},styles.textPadding]} 
-      source={require('../../images/Ivan-Vazov.png')}></Image>
-      <Text style={styles.quoteStyle}>"Книгите са най-евтината ваканция, която може да си купите."</Text>
-        
+      <TouchableWithoutFeedback onLongPress={()=>this.record()}>
+        <Image style={[{width: 232.32, height: 258.94, alignSelf: 'center'},styles.textPadding]} 
+          source={require('../../images/Ivan-Vazov.png')}></Image>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onLongPress={()=>this.record()}>
+        <Text style={styles.quoteStyle}>"Книгите са най-евтината ваканция, която може да си купите."</Text>
+      </TouchableWithoutFeedback>
         
         <View style={{flexDirection:'row'}}>
-
-        <Button
-          color="green"
-          title="test sound"
-          onPress={() => {
-            
-          }}
-      />
-
         <TouchableOpacity onPress={() => {
           this.props.navigation.push('Authors')}}>
         <Image style={[{width: 120, height: 120}]} 
@@ -64,6 +85,7 @@ export default class DetailsScreen extends Component {
         </View>
 
       </ImageBackground>
+      
     );
   }
 }
